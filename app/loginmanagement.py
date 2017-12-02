43ab12574b315f4dd81cd245585c426631a6a280
Ryan Chen <c.ryanchen@gmail.com>
2017-12-02 14:22:41 -0500

    clean up for login things, persistent login

--- app/loginmanagement.py
+++ app/loginmanagement.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, escape
 from flaskext.mysql import MySQL
 import flask_login
 
-#mySQL loading (might want to keep it in the separate py files though)
+#mySQL loading
 mysql = MySQL()
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -16,6 +16,7 @@ conn = mysql.connect()
 cursor = conn.cursor()
 cursor.execute("SELECT email FROM Users")
 users = cursor.fetchall()
+
 
 def getUsers():
     cursor = conn.cursor()
@@ -64,3 +65,65 @@ def isEmailUnique(email):
         return False
     else:
         return True
+    
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'></input>
+                <input type='password' name='password' id='password' placeholder='password'></input>
+                <input type='submit' name='submit'></input>
+               </form></br>
+           <a href='/'>Home</a>
+               '''
+    #The request method is POST (page is recieving data)
+    email = request.form['email']
+    cursor = conn.cursor()
+    #check if email is registered
+    if cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email)):
+        data = cursor.fetchall()
+        pwd = str(data[0][0] )
+        if request.form['password'] == pwd:
+            user = User()
+            user.id = email
+            flask_login.login_user(user) #okay login in user
+            return render_template('index.html', message='Logged in!')
+ 
+
+    #information did not match
+    return "<a href='/login'>Try again</a>\
+            </br><a href='/register'>or make an account</a>"
+
+def logout():
+    flask_login.logout_user()
+    return render_template('index.html', message='Logged out')
+
+def unauthorized_handler():
+    return render_template('unauth.html')
+
+def register():
+    return render_template('register.html', supress='True')
+
+def register_user():
+    try:
+        username=request.form.get('name')
+        email=request.form.get('email')
+        password=request.form.get('password')
+    except:
+        print("couldn't find all tokens")
+        return flask.redirect(flask.url_for('register'))
+    cursor = conn.cursor()
+    unique =  isEmailUnique(email)
+    if unique:
+        cursor.execute("INSERT INTO Users (username, email, password) VALUES ('{0}', '{1}', '{2}')".format(username, email, password))
+        conn.commit()
+        #log user in
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return render_template('index.html', name=username, message='Account Created!')
+    else:
+        print("couldn't find all tokens")
+        return render_template("register.html", suppress=False)
+
+
